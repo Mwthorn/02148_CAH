1b7fcb5612048c4088998e00c65a57b714b186d0
Solikiea <35102843+Solikiea@users.noreply.github.com>
2018-01-04 15:17:46 +0100

    client/server

--- jSpace-Project/src/common/src/main/Client.java
+++ jSpace-Project/src/common/src/main/Client.java
@@ -1,7 +1,26 @@
 package common.src.main;
 
-public class Client {
-    public static void main(String[] argv) {
+import java.io.IOException;
+import java.net.UnknownHostException;
 
+import org.jspace.ActualField;
+import org.jspace.FormalField;
+import org.jspace.RemoteSpace;
+import org.jspace.SequentialSpace;
+import org.jspace.SpaceRepository;
+
+public class Client {
+    public static void main(String[] args) {
+        RemoteSpace lobby;
+        
+			try {
+				lobby = new RemoteSpace("tcp://127.0.0.1:9001/chat?keep");
+				lobby.put("enter",0);
+				
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
     }
 }
--- jSpace-Project/src/common/src/main/Server.java
+++ jSpace-Project/src/common/src/main/Server.java
@@ -1,10 +1,30 @@
 package common.src.main;
 
+import org.jspace.ActualField;
+import org.jspace.FormalField;
+import org.jspace.SequentialSpace;
+import org.jspace.SpaceRepository;
+
 public class Server {
     public static void main(String[] argv) {
-
-
-
-
+    	
+    	// initialisation();
+    	
+    	SequentialSpace lobby = new SequentialSpace();     // Authentication/Lobby tuple
+		//SequentialSpace game = new SequentialSpace();      // Game tuple
+		        	
+		SpaceRepository repository = new SpaceRepository();
+		repository.addGate("tcp://127.0.0.1:9001/?keep");
+		repository.add("lobby", lobby);
+		//repository.add("game", game);
+		
+		try {
+			Object[] tuple = lobby.get(new FormalField(String.class));
+			
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
     }
 }
