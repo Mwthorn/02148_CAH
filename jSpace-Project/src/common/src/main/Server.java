248f1e5839ba12371fd62e58c56d990970887978
Solikiea <35102843+Solikiea@users.noreply.github.com>
2018-01-04 15:57:26 +0100

    client/server

--- jSpace-Project/src/common/src/main/Server.java
+++ jSpace-Project/src/common/src/main/Server.java
@@ -1,5 +1,7 @@
 package common.src.main;
 
+import java.util.Random;
+
 import org.jspace.ActualField;
 import org.jspace.FormalField;
 import org.jspace.SequentialSpace;
@@ -9,6 +11,10 @@ public class Server {
     public static void main(String[] argv) {
     	
     	// initialisation();
+    	int maxUsers = 20;
+    	int numberOfUsers = 0;
+    	int[] id = new int[maxUsers];
+    	String[] name = new String[maxUsers];
     	
     	SequentialSpace lobby = new SequentialSpace();     // Authentication/Lobby tuple
 		//SequentialSpace game = new SequentialSpace();      // Game tuple
@@ -19,8 +25,14 @@ public class Server {
 		//repository.add("game", game);
 		
 		try {
-			Object[] tuple = lobby.get(new FormalField(String.class));
+			Object[] tuple = lobby.get(new ActualField("enter"), new FormalField(String.class),
+					new FormalField(Integer.class));
 			
+			// Creating a random user ID
+			Random rand = new Random();
+			int  n = rand.nextInt(99999) + 10000;
+			id[numberOfUsers] = n;
+			name[numberOfUsers] = (String) tuple[1];
 			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
