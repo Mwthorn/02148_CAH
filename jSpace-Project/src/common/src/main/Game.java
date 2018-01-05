5c101684516c05eb5fe80df25369197652988b82
Mwthorn <mwthorn@gmail.com>
2018-01-05 12:28:29 +0100

    Added Game object

---
+++ jSpace-Project/src/common/src/main/Game.java
@@ -0,0 +1,16 @@
+package common.src.main;
+
+import common.src.main.cards.BlackCard;
+import common.src.main.cards.WhiteCard;
+
+public class Game {
+    // Players in game
+    // Password
+    // Cards
+
+    private String gameName;
+    private String password;
+    private WhiteCard[] whiteCards;
+    private BlackCard[] blackCards;
+
+}
