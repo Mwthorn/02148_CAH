8c496bf7dbede5ba48661a8781e5d0e9a026f197
Mwthorn <mwthorn@gmail.com>
2018-01-05 12:59:28 +0100

    Methods for Game and Player

--- jSpace-Project/src/common/src/main/Game.java
+++ jSpace-Project/src/common/src/main/Game.java
@@ -3,14 +3,73 @@ package common.src.main;
 import common.src.main.cards.BlackCard;
 import common.src.main.cards.WhiteCard;
 
+import java.util.ArrayList;
+
 public class Game {
-    // Players in game
-    // Password
-    // Cards
 
     private String gameName;
     private String password;
     private WhiteCard[] whiteCards;
     private BlackCard[] blackCards;
+    private ArrayList<Player> players;
+    private String status;
+    // TODO: Add chat?
+    // TODO: Add rounds?
+    // TODO: Add host?
+    // TODO: Add vote count?
 
+    public Game(String gameName, WhiteCard[] whiteCards, BlackCard[] blackCards) {
+        this.gameName = gameName;
+        this.whiteCards = whiteCards;
+        this.blackCards = blackCards;
+        this.password = null;
+    }
+
+    public Game(String gameName, WhiteCard[] whiteCards, BlackCard[] blackCards, String password) {
+        this.gameName = gameName;
+        this.whiteCards = whiteCards;
+        this.blackCards = blackCards;
+        this.password = password;
+    }
+
+    public String getGameName() {
+        return this.gameName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public boolean hasPassword() {
+        return this.password != null;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return this.players;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void addPointsTo(Player player, int i) {
+        player.addPoints(i);
+    }
+
+    public void addPlayerToGame(Player player) {
+        players.add(player);
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public WhiteCard pullRandomWhiteCard() {
+        
+        return
+    }
 }
