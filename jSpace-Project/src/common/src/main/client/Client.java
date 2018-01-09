0d3168d265f69b6a2e8f2c4ad2c39cde53da717a
J. Ulveman <jonas_ulveman@hotmail.com>
2018-01-09 13:22:44 +0100

    Login and other things

--- jSpace-Project/src/common/src/main/client/Client.java
+++ jSpace-Project/src/common/src/main/client/Client.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-import common.src.main.philosopher1;
 import common.src.main.server.Game;
 import common.src.main.server.Player;
 import org.jspace.ActualField;
