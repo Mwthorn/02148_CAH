fe9e8c28ca49f0c2cac5429e3f064a108302777d
J. Ulveman <jonas_ulveman@hotmail.com>
2018-01-09 11:28:10 +0100

    Login Doneish

--- jSpace-Project/src/common/src/main/gui/Login.java
+++ jSpace-Project/src/common/src/main/gui/Login.java
@@ -8,12 +8,6 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -42,6 +36,12 @@ public class Login extends JFrame implements ActionListener {
 		Dimension btnsize4 = new Dimension(110,40);
 		Dimension btnsize2 = new Dimension(120,60);
 
+		//Textfield laves
+		Dimension txtfldsize = new Dimension(400, 30);
+		
+		//Labels
+		Dimension lsize = new Dimension(99, 50);
+		
 		// Make Buttons
 		BQuit = new JButton("Quit");
 		BQuit.setMaximumSize(btnsize2);
@@ -77,9 +77,6 @@ public class Login extends JFrame implements ActionListener {
 		LText.setFont(new Font("calibri",Font.PLAIN,30));
 		LText.setForeground(Color.black);
 		
-		//Labels
-		Dimension lsize = new Dimension(99, 50);
-
 		LName = new JLabel("Name:");
 		LName.setMaximumSize(lsize);
 		LName.setFont(new Font("calibri",Font.PLAIN,25));
@@ -99,6 +96,17 @@ public class Login extends JFrame implements ActionListener {
 		LFigure1.setAlignmentX(Component.CENTER_ALIGNMENT);
 		LFigure2.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+		txtfld1 = new JTextField(50);
+		txtfld1.setMaximumSize(txtfldsize);
+		txtfld1.setFont(new Font("calibri",Font.PLAIN,20));
+		txtfld1.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		txtfld2 = new JTextField(50);
+		txtfld2.setMaximumSize(txtfldsize);
+		txtfld2.setFont(new Font("calibri",Font.PLAIN,20));
+		txtfld2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+
 		
 		JPanel PC = new JPanel();
 		PC.setLayout(new BorderLayout());
@@ -108,9 +116,18 @@ public class Login extends JFrame implements ActionListener {
 		JPanel PCenter = new JPanel();
 		PCenter.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.black));			
 		PCenter.setLayout(new BorderLayout()); //Default layout
-		PCenter.setSize(700,400);
 		PCenter.setBackground(Color.BLACK);
 
+		JPanel PCCenter = new JPanel();
+		PCCenter.setLayout(new BoxLayout(PCCenter, BoxLayout.PAGE_AXIS));
+		PCCenter.add(Box.createRigidArea(new Dimension(100, 20)));
+		PCCenter.add(LText);
+		PCCenter.add(Box.createRigidArea(new Dimension(100, 20)));
+		PCCenter.add(txtfld1, BorderLayout.CENTER);
+		PCCenter.add(Box.createRigidArea(new Dimension(100, 40)));
+		PCCenter.add(txtfld2, BorderLayout.CENTER);
+
+		PCenter.add(PCCenter, BorderLayout.CENTER);
 		
 		//Button inside
 		JPanel PCRight = new JPanel();
@@ -122,33 +139,9 @@ public class Login extends JFrame implements ActionListener {
 		PCRight.add(Box.createRigidArea(new Dimension(150, 40)));
 		PCenter.add(PCRight, BorderLayout.EAST);
 
-		//Textfield laves
-		Dimension txtfldsize = new Dimension(400, 30);
-
-		txtfld1 = new JTextField(50);
-		txtfld1.setMaximumSize(txtfldsize);
-		txtfld1.setFont(new Font("calibri",Font.PLAIN,20));
-		txtfld1.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-		txtfld2 = new JTextField(50);
-		txtfld2.setMaximumSize(txtfldsize);
-		txtfld2.setFont(new Font("calibri",Font.PLAIN,20));
-		txtfld2.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-		JPanel PCCenter = new JPanel();
-		PCCenter.setLayout(new BoxLayout(PCCenter, BoxLayout.PAGE_AXIS));
-		PCCenter.add(Box.createRigidArea(new Dimension(10, 20)));
-		PCCenter.add(LText);
-		PCCenter.add(Box.createRigidArea(new Dimension(10, 20)));
-		PCCenter.add(txtfld1, BorderLayout.CENTER);
-		PCCenter.add(Box.createRigidArea(new Dimension(10, 40)));
-		PCCenter.add(txtfld2, BorderLayout.CENTER);
-
-		PCenter.add(PCCenter, BorderLayout.CENTER);
-
 		JPanel PCLeft = new JPanel();
 		PCLeft.setLayout(new BoxLayout(PCLeft, BoxLayout.PAGE_AXIS));
-		PCLeft.add(Box.createRigidArea(new Dimension(150, 60)));
+		PCLeft.add(Box.createRigidArea(new Dimension(150, 68)));
 		PCLeft.add(LName);
 		PCLeft.add(Box.createRigidArea(new Dimension(150, 20)));
 		PCLeft.add(LIP);
@@ -158,7 +151,7 @@ public class Login extends JFrame implements ActionListener {
 		PCLeft.setBackground(Color.WHITE);
 		
 		PCenter.add(PCLeft, BorderLayout.WEST);
-		PCenter.setSize(400, 1000);
+//		PCenter.setSize(400, 1000);
 	
 		
 		
@@ -199,10 +192,9 @@ public class Login extends JFrame implements ActionListener {
 		PC.add(PL, BorderLayout.WEST);
 		PC.add(PR, BorderLayout.EAST);
 		
-		PL.add(Box.createRigidArea(new Dimension(100,50)));
-		PR.add(Box.createRigidArea(new Dimension(100,50)));
+		PL.add(Box.createRigidArea(new Dimension(150,100)));
+		PR.add(Box.createRigidArea(new Dimension(150,100)));
 
-		
 		PC.setBackground(Color.WHITE);
 		PC.add(PCenter);
 		
@@ -213,9 +205,9 @@ public class Login extends JFrame implements ActionListener {
 		PMiddle.add(LTitle);
 		PMiddle.add(Box.createRigidArea(new Dimension(400, 100)));
 		
-		PMiddle.add(Box.createRigidArea(new Dimension(400, 100)));
+		PMiddle.add(Box.createRigidArea(new Dimension(400, 150)));
 		PMiddle.add(PC);
-		PMiddle.add(Box.createRigidArea(new Dimension(400, 100)));				
+		PMiddle.add(Box.createRigidArea(new Dimension(400, 800)));				
 				
 		//Add PMiddle and set center
 		getContentPane().add(PMiddle, BorderLayout.CENTER);
@@ -226,6 +218,9 @@ public class Login extends JFrame implements ActionListener {
 		
 	}
 	
+	String name;
+	String IP;
+	
 	public void actionPerformed(ActionEvent e){
 
 		/////////////////////////////////////////////////////////////////
@@ -234,8 +229,21 @@ public class Login extends JFrame implements ActionListener {
 			
 			System.exit(0);
 			
+		} else if (e.getSource() == BSignIn){
+			this.name = txtfld1.getText();
+			this.IP = txtfld2.getText();
+			
 		}
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getIP() {
+		return IP;
+	}
+	
 	
 	public static void main(String[] args) {
 	
