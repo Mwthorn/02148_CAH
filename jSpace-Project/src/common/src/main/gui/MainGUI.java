0ad09e49334e1ae04e1b19dfc7636451f2f9e4df
YC <yael_choleva@hotmail.com>
2018-01-16 17:32:39 +0100

    chat størresle

--- jSpace-Project/src/common/src/main/gui/MainGUI.java
+++ jSpace-Project/src/common/src/main/gui/MainGUI.java
@@ -1359,7 +1359,7 @@ public class MainGUI extends JFrame implements ActionListener {
 
 
 		chatPanel.setLayout(new BorderLayout());
-		chatPanel.setPreferredSize(new Dimension(0,600));
+		chatPanel.setPreferredSize(new Dimension(0,100));
 		chatPanel.setVisible(true);
 		sendPanel.setBackground(Color.WHITE);
 		sendPanel.setLayout(new GridBagLayout());
@@ -1867,8 +1867,7 @@ public class MainGUI extends JFrame implements ActionListener {
 			mainLobby.requestFocus();
 			Client.sendLeave();
 			updateGameList();
-		}
-		else if (e.getSource() == BReady) {
+		} else if (e.getSource() == BReady) {
 			Client.sendReady();
 		} else if ( e.getSource() == lobbySendButton ) {
 			if (lobbyMessageField.getText().length() < 1) {
